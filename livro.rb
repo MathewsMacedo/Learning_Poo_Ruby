7b8340778c7f8f41b2731f74ae4780efcb32db40
Matheus Macedo <matheus.smacedo@hotmail.com>
2019-09-27 10:58:30 -0300

    Separado em classes e trabalhando com lambda

--- livro.rb
+++ livro.rb
@@ -1,13 +1,14 @@
 class Livro
 
-	attr_reader :titulo,:preco, :ano_lacamento
+	attr_reader :titulo,:preco, :ano_lacamento, :editora, :tipo
 
-	def initialize(titulo,preco,ano_lacamento, possui_reimpressao)
+	def initialize(titulo,preco,ano_lacamento, possui_reimpressao,editora,tipo)
 		@titulo = titulo
 		@possui_reimpressao = possui_reimpressao 
 		@ano_lacamento = ano_lacamento
 		@preco = calcula_desconto(preco)
-		
+		@editora = editora
+		@tipo = tipo
 	end
 
 	def possui_reimpressao?
@@ -34,86 +35,3 @@ class Livro
 end
 
 
-module Contador
-
-	def <<(livro)
-		push(livro)
-	 if @maximo_necessario.nil? || @maximo_necessario < size
-		@maximo_necessario = size
-	 end
-		self
-
-	end
-	def maximo_necessario
-		@maximo_necessario
-	end
-
-
-end
-
-
-class Estoque
-
-
-
-
-	def initialize
-
-		@livros = []
-		@livros.extend Contador
-
-	end
-
-
-	def << (livro)
-		@livros << livro if livro
-	end
-
-	def remove (livro)
-		@livros.delete livro
-	end
-
-	def maximo_necessario
-		@livros.maximo_necessario
-	end
-
-
-	def total
-		@livros.size
-	end
-
-
-	def mais_barato_que(valor)
-  
-		@livros.select do |livro| 
-			livro.preco < valor
-		end
-
-	end
-
-	def exporta_csv
-
-		@livros.each do |livro|
-			livro.to_csv
-		end
-	end
-
-end
-
-	estoque = Estoque.new
-
-	 
-algoritmo = Livro.new("Agile Web Development with Rails",100.00,1998,false)
-arquitetura= Livro.new("Introdução À Arquitetura e Design de Software", 70.00,  2011, true)
- 
- estoque << algoritmo
- puts estoque.maximo_necessario
- estoque << arquitetura
- puts estoque.maximo_necessario
-estoque.remove algoritmo
- 
-puts estoque.maximo_necessario
-
-puts estoque.total
-puts estoque.mais_barato_que 80
-estoque.exporta_csv
